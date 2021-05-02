65292e4605b0795317f4dbd7b142ea6645301424
kartikkumargujarati <kgujarati210@gmail.com>
2021-05-02 12:44:38 -0500

    datasource and resolver setup for roadster service

--- src/company-service/resolvers.ts
+++ src/company-service/resolvers.ts
@@ -5,4 +5,5 @@ const resolvers = {
     }
   }
 };
+
 export { resolvers };
---
+++ src/roadster-service/resolvers.ts
@@ -0,0 +1,12 @@
+import { getRoaster } from './reducer';
+
+const resolvers = {
+  Query: {
+    roadster: async (_source: any, _: any, { dataSources }: any) => {
+      const roadsterInfo = await dataSources.roadsterAPI.getRoadsterInfo();
+      return getRoaster(roadsterInfo);
+    }
+  }
+};
+
+export { resolvers };
