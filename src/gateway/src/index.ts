6d797b50df19efe65039a179af95fbefa1052222
kartikkumargujarati <kgujarati210@gmail.com>
2021-05-09 15:12:39 -0500

    enable introspection at gateway and add api key

--- src/gateway/src/index.ts
+++ src/gateway/src/index.ts
@@ -5,7 +5,15 @@ const PORT = process.env.PORT || 6969;
 
 const gateway = new ApolloGateway();
 
-const server = new ApolloServer({ gateway, subscriptions: false, tracing: true });
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+  tracing: true,
+  introspection: true,
+  engine: {
+    apiKey: process.env.APOLLO_KEY
+  }
+});
 
 server.listen({ port: PORT }).then(({ url }) => {
   console.log(`🚀 Gateway ready at url: ${url}`);
