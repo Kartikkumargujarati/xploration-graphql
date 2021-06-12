189c9862aafcd2fa66d2b3e385f8ae0753ea4dcd
kartikkumargujarati <kgujarati210@gmail.com>
2021-06-12 18:10:46 -0500

    detele the old src dir

--- src/company-service/src/resolvers.ts
+++
@@ -1,9 +0,0 @@
-const resolvers = {
-    Query: {
-        company: async (_source: any, _: any, { dataSources }: any) => {
-            return await dataSources.companyAPI.getCompanyInfo();
-        }
-    }
-};
-
-export { resolvers };
--- src/rocket-service/src/dto.ts
+++
@@ -1,8 +0,0 @@
-export interface RocketDto {
-    id: string;
-    description: string;
-    company: string;
-    country: string;
-    name: string;
-    flickr_images: string[];
-}
